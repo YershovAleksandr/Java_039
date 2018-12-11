e4f57fa9d1006ab15fe15e116d7e47406862825a
hp <namstudionsk@gmail.com>
2018-12-11 23:19:51 +0700

    Fix

--- src/main/java/com/nam/App.java
+++ src/main/java/com/nam/App.java
@@ -6,12 +6,26 @@ package com.nam;
  */
 public class App 
 {
-    public static void main( String[] args )
+    public static void main( String[] args ) throws Exception
     {
         System.out.println( "Hello World!" );
 
         Foo foo = new Foo();
 
         foo.run();
+
+        BuzFactory bf = new BuzFactory();
+
+        //AbstractBuz buz = bf.getBuz();
+        AbstractBuz buz = bf.getBuz();
+
+        System.out.println("2ch = " + buz.read2ch());
+
+        /*public String toString(){
+        return this.getClass().getField("representation").get(this);
+        }*/
+
+        //int i = buz.getClass().getMethod("read2ch").invoke(this);
+
     }
 }
