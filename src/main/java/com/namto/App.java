e0df79ea8acc2abe6009fc3595c1988b791493b6
hp <namstudionsk@gmail.com>
2018-12-25 23:35:47 +0700

    Work in progress

--- src/main/java/com/namto/App.java
+++ src/main/java/com/namto/App.java
@@ -5,5 +5,26 @@ public class App {
 
         System.out.println("Hello World!");
 
+        double res = 0.;
+
+        try {
+            res = Stuff.run(0);
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+
+        System.out.println("Res = " + res);
+    }
+
+    @Override
+    public boolean equals(Object o){
+        if (this == o){
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()){
+            return false;
+        }
+
+        return false;
     }
 }
