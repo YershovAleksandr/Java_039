d9c0492c44ebc46e9acca9a9555dafa2f78deb29
hp <namstudionsk@gmail.com>
2019-03-01 00:57:53 +0700

    fix

--- src/main/java/com/nam/App.java
+++ src/main/java/com/nam/App.java
@@ -1,5 +1,16 @@
 package com.nam;
 
+
+import javax.json.Json;
+import javax.json.stream.JsonParser;
+import static javax.json.stream.JsonParser.Event;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+
 /**
  * Hello world!
  *
@@ -9,5 +20,36 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World 42!" );
+
+        final String PATH = "https://api.hh.ru/areas/countries";
+
+        URL url = null;
+
+        try {
+            url = new URL(PATH);
+        }catch (MalformedURLException e){
+            e.printStackTrace();
+        }
+
+        try (InputStream is = url.openStream(); JsonParser parser = Json.createParser(is)){
+            while(parser.hasNext()){
+                Event e = parser.next();
+
+                if (e == Event.KEY_NAME) {
+                    switch (parser.getString()) {
+                        case "id":
+                            parser.next();
+                            System.out.println(parser.getString());
+                            break;
+                        case "name":
+                            parser.next();
+                            System.out.println(parser.getString());
+                            break;
+                    }
+                }
+            }
+        }catch (IOException e){
+            e.printStackTrace();
+        }
     }
 }
