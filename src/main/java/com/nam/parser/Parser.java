e48e15d5d29105b36da18d86b22235b52afb6f1d
hp <namstudionsk@gmail.com>
2019-03-03 16:09:31 +0700

    Refactored to use ParseReader

--- src/main/java/com/nam/parser/Parser.java
+++ src/main/java/com/nam/parser/Parser.java
@@ -4,7 +4,6 @@ import javax.json.Json;
 import javax.json.JsonArray;
 import javax.json.JsonObject;
 import javax.json.JsonReader;
-import java.io.IOException;
 import java.io.InputStream;
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -12,63 +11,84 @@ import java.net.URL;
 public class Parser {
 
     //final private String URL_PATH = "https://api.hh.ru/vacancies?area=1202&specialization=1&per_page=100&page=0";
-    private String URL_PATH_FORMAT = "https://api.hh.ru/vacancies?area=%d&specialization=%d&per_page=%d&page=%d";
+    final private String URL_PATH_FORMAT = "https://api.hh.ru/vacancies?area=%d&specialization=%d&per_page=%d&page=%d";
+    final private String KEY_FOUND = "found";
+    final private String KEY_ITEMS = "items";
+    final private String KEY_NAME = "name";
+    final private String KEY_SALARY = "salary";
+    final private String KEY_FROM = "from";
+    final private String KEY_TO = "to";
+    final private String KEY_CURRENCY = "currency";
+    final private String KEY_GROSS = "gross";
+    final private String KEY_PUBLISHED_AT = "published_at";
+    final private String KEY_EMPLOYER = "employer";
+
+    private ParseReader parseReader;
 
     private int area;
     private int specialization;
+    private int found;
 
-    public Parser(int area, int specialization){
+    public Parser(ParseReader parseReader, int area, int specialization){
+        this.parseReader = parseReader;
         this.area = area;
         this.specialization = specialization;
     }
 
+    //TODO ???
+    public int getFound(){
+        return found;
+    }
+
+    //TODO ???
+    public void parse(){
+
+    }
+
     public void parse(int per_page, int page){
-        URL url = null;
+        URL url;
 
         try {
             url = new URL(String.format(URL_PATH_FORMAT, area, specialization, per_page, page));
         }catch (MalformedURLException e){
             e.printStackTrace();
+
+            return;
         }
 
         try(InputStream is = url.openStream(); JsonReader rdr = Json.createReader(is)){
             JsonObject obj = rdr.readObject();
 
-            System.out.println("found " + obj.getInt("found"));
-            System.out.println("per_page " + obj.getInt("per_page"));
-            System.out.println("page " + obj.getInt("page"));
+            found = obj.getInt(KEY_FOUND);
 
-            JsonArray results = obj.getJsonArray("items");
+            JsonArray results = obj.getJsonArray(KEY_ITEMS);
 
-            int i = 0;
             for (JsonObject result : results.getValuesAs(JsonObject.class)){
-                System.out.println();
-                System.out.print(++i + " " +  result.getString("name"));
-                if (!result.isNull("salary")) {
-                    if (!result.getJsonObject("salary").isNull("from")) {
-                        System.out.print(" " + result.getJsonObject("salary").getInt("from"));
+                parseReader.setName(result.getString(KEY_NAME));
+                parseReader.setPublishedAt(result.getString(KEY_PUBLISHED_AT));
+                parseReader.setEmployerName(result.getJsonObject(KEY_EMPLOYER).getString(KEY_NAME));
+
+                if (!result.isNull(KEY_SALARY)) {
+                    if (!result.getJsonObject(KEY_SALARY).isNull(KEY_FROM)) {
+                        parseReader.setSalaryFrom(result.getJsonObject(KEY_SALARY).getInt(KEY_FROM));
                     }
 
-                    if (!result.getJsonObject("salary").isNull("to")){
-                        System.out.print(" " + result.getJsonObject("salary").getInt("to"));
+                    if (!result.getJsonObject(KEY_SALARY).isNull(KEY_TO)){
+                        parseReader.setSalaryTo(result.getJsonObject(KEY_SALARY).getInt(KEY_TO));
                     }
 
-                    if (!result.getJsonObject("salary").isNull("currency")) {
-                        System.out.print(" " + result.getJsonObject("salary").getString("currency"));
+                    if (!result.getJsonObject(KEY_SALARY).isNull(KEY_CURRENCY)) {
+                        parseReader.setSalaryCurrency(result.getJsonObject(KEY_SALARY).getString(KEY_CURRENCY));
                     }
 
-                    if (!result.getJsonObject("salary").isNull("gross")) {
-                        System.out.print(" " + result.getJsonObject("salary").getBoolean("gross"));
+                    if (!result.getJsonObject(KEY_SALARY).isNull(KEY_GROSS)) {
+                        parseReader.setSalaryGross(result.getJsonObject(KEY_SALARY).getBoolean(KEY_GROSS));
                     }
-
-                    System.out.print(" " + result.getString("published_at"));
-
-                    System.out.print(" " + result.getJsonObject("employer").getString("name"));
                 }
 
-
+                parseReader.process();
             }
-        }catch (IOException e){
+        }catch (Exception e){
             e.printStackTrace();
         }
 
