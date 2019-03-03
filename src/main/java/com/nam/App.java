ab0f6dc1132908a7a8de9dbe959094bdf76476b1
hp <namstudionsk@gmail.com>
2019-03-03 14:34:33 +0700

    Fix

--- src/main/java/com/nam/App.java
+++ src/main/java/com/nam/App.java
@@ -1,20 +1,6 @@
 package com.nam;
 
-
 import com.nam.parser.Parser;
-
-import javax.json.Json;
-import javax.json.JsonArray;
-import javax.json.JsonObject;
-import javax.json.JsonReader;
-import javax.json.stream.JsonParser;
-import static javax.json.stream.JsonParser.Event;
-
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.MalformedURLException;
-import java.net.URL;
-
 
 /**
  * Hello world!
