bc7d57d69c90552fc052e7d89a672cd12aa82c06
hp <namstudionsk@gmail.com>
2019-03-03 14:31:30 +0700

    Refactored for use Parse class

--- src/main/java/com/nam/App.java
+++ src/main/java/com/nam/App.java
@@ -1,6 +1,8 @@
 package com.nam;
 
 
+import com.nam.parser.Parser;
+
 import javax.json.Json;
 import javax.json.JsonArray;
 import javax.json.JsonObject;
@@ -24,79 +26,11 @@ public class App
     {
         System.out.println( "Hello World 42!" );
 
-        //final String PATH = "https://api.hh.ru/areas/countries";
-        final String PATH = "https://api.hh.ru/vacancies?area=1202&specialization=1&per_page=100&page=0";
+        Parser parser = new Parser(1202, 1);
 
-        URL url = null;
+        parser.parse(10, 0);
+        parser.parse(10, 1);
+        parser.parse(10, 2);
 
-        try {
-            url = new URL(PATH);
-        }catch (MalformedURLException e){
-            e.printStackTrace();
-        }
-
-        try(InputStream is = url.openStream(); JsonReader rdr = Json.createReader(is)){
-            JsonObject obj = rdr.readObject();
-
-            System.out.println("found " + obj.getInt("found"));
-            System.out.println("per_page " + obj.getInt("per_page"));
-            System.out.println("page " + obj.getInt("page"));
-
-            JsonArray results = obj.getJsonArray("items");
-
-            int i = 0;
-            for (JsonObject result : results.getValuesAs(JsonObject.class)){
-                System.out.println();
-                System.out.print(++i + " " +  result.getString("name"));
-                if (!result.isNull("salary")) {
-                    if (!result.getJsonObject("salary").isNull("from")) {
-                        System.out.print(" " + result.getJsonObject("salary").getInt("from"));
-                    }
-
-                    if (!result.getJsonObject("salary").isNull("to")){
-                        System.out.print(" " + result.getJsonObject("salary").getInt("to"));
-                    }
-
-                    if (!result.getJsonObject("salary").isNull("currency")) {
-                        System.out.print(" " + result.getJsonObject("salary").getString("currency"));
-                    }
-
-                    if (!result.getJsonObject("salary").isNull("gross")) {
-                        System.out.print(" " + result.getJsonObject("salary").getBoolean("gross"));
-                    }
-
-                    System.out.print(" " + result.getString("published_at"));
-
-                    System.out.print(" " + result.getJsonObject("employer").getString("name"));
-                }
-
-
-            }
-        }catch (IOException e){
-            e.printStackTrace();
-        }
-
-        System.out.println();
-
-        /*try (InputStream is = url.openStream(); JsonParser parser = Json.createParser(is)){
-            while(parser.hasNext()){
-                Event e = parser.next();
-
-                if (e == Event.KEY_NAME) {
-                    switch (parser.getString()) {
-                        case "id":
-                            parser.next();
-                            System.out.println(parser.getString());
-                            break;
-                        case "name":
-                            parser.next();
-                            System.out.println(parser.getString());
-                            break;
-                    }
-                }
-            }
-        }catch (IOException e){
-            e.printStackTrace();
-        }*/
     }
 }
